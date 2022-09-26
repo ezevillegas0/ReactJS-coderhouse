658b6829b3f0c8973fb3b2aa34c7c1fd3a658318
ezequiel villegas <ezevillegas0@gmail.com>
2022-09-26 11:33:27 -0300

    trabajo en la clase 3 de react

---
+++ src/componentes/AvatarJsx/AvatarJsx.js
@@ -0,0 +1,14 @@
+import './AvatarJsx.css'
+
+const AvatarJsx = () => {
+    const src = `https://randomuser.me/api/portraits/lego/2.jpg`
+
+    return (
+        <picture>
+            <img src={src}  alt='avatar'/>
+            Andres
+        </picture>
+    )
+}
+
+export default AvatarJsx
---
+++ src/componentes/NavBar/navBar.js
@@ -0,0 +1,15 @@
+import "./navBar.css"
+
+const NavBar = () => {
+    return (
+        <nav class="navNavbar">
+            <h1 class="tituloNavBar"> Venta de sneakers</h1>
+            <div >
+                <button>Zapatillas</button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default NavBar
