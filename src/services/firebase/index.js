d906bf4a35b6a3b340751e0bf7e5a068da3b0dfd
ezequiel villegas <ezevillegas0@gmail.com>
2022-10-30 12:14:07 -0300

    cambio las variables de entorno

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1kzI4cy4qIydlpbNnOqcntCX84Wy_WRo",
-  authDomain: "coderhouse-react-7f5e6.firebaseapp.com",
-  projectId: "coderhouse-react-7f5e6",
-  storageBucket: "coderhouse-react-7f5e6.appspot.com",
-  messagingSenderId: "1064694125247",
-  appId: "1:1064694125247:web:8e21cb7f9eedefd150ec0a"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
